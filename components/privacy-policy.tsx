0d5e5a859e14da1febcb340e4b31bd3bfb111d4a
Alamgir Ahamed Saif <36258593+alamgir72435@users.noreply.github.com>
2022-09-14 22:02:50 +0600

    test

--- components/privacy-policy.tsx
+++ components/privacy-policy.tsx
@@ -90,7 +90,7 @@ const PrivacyPolicy = () => {
         I want to inform you that whenever you use my Service, in a case of an
         error in the app I collect data and information (through third-party
         products) on your phone called Log Data. This Log Data may include
-        information such as your device Internet Protocol (“IP”) address, device
+        information such as your device Internet Protocol (IP) address, device
         name, operating system version, the configuration of the app when
         utilizing my Service, the time and date of your use of the Service, and
         other statistics.
@@ -101,11 +101,11 @@ const PrivacyPolicy = () => {
       <p>
         Cookies are files with a small amount of data that are commonly used as
         anonymous unique identifiers. These are sent to your browser from the
-        websites that you visit and are stored on your device's internal memory.
+        websites that you visit and are stored on your devices internal memory.
       </p>{" "}
       <p>
-        This Service does not use these “cookies” explicitly. However, the app
-        may use third-party code and libraries that use “cookies” to collect
+        This Service does not use these cookies explicitly. However, the app may
+        use third-party code and libraries that use cookies to collect
         information and improve their services. You have the option to either
         accept or refuse these cookies and know when a cookie is being sent to
         your device. If you choose to refuse our cookies, you may not be able to
